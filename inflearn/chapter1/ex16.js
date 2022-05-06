0d1d9db815156e452278a05e6e377ab62bb1d190
99-zziy <wldud8229@gmail.com>
2022-05-06 19:07:16 +0900

    chapter1 - ex16 문제풀이

---
+++ inflearn/chapter1/ex16.js
@@ -0,0 +1,26 @@
+/*
+- 중복문자제거
+
+소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하세요.
+제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+
+▣ 입력설명
+첫 줄에 문자열이 입력됩니다.
+
+▣ 출력설명
+첫 줄에 중복문자가 제거된 문자열을 출력합니다.
+
+*/
+
+const solution = (str) => {
+  let strArray = str.split("");
+  let newArray = [];
+  strArray.map((str) => {
+    if (!newArray.includes(str)) {
+      newArray.push(str);
+    }
+  });
+  return newArray.join("");
+};
+
+console.log(solution("ksekkset"));
