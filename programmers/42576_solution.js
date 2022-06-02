d3f4d6f187bf614dca2066f19ebf8291af845214
zzi <62633444+99-zziy@users.noreply.github.com>
2022-06-02 22:33:29 +0900

    Create 42576_solution.js

---
+++ programmers/42576_solution.js
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42576
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+
+    for(let i in participant) {
+        if(participant[i] !== completion[i]) return participant[i];
+    }
+    
+}
