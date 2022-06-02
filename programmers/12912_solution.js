cc6d5ec84e1d993ea537eb41d792ae16e6fa26dc
zzi <62633444+99-zziy@users.noreply.github.com>
2022-06-02 22:50:09 +0900

    Create 12912_solution.js

---
+++ programmers/12912_solution.js
@@ -0,0 +1,20 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12912
+function solution(a, b) {
+    let bigNum = a ;
+    let smallNum = b;
+    let answer = 0;
+
+    if (a < b) {
+        bigNum = b;
+        smallNum = a;   
+    }
+    if (a === b)
+        answer = a;
+    else {
+        for (i = smallNum; i<= bigNum; i++){
+            answer = answer + i
+        }   
+    }
+
+    return answer;
+}
