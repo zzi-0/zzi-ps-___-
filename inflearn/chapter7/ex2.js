c83fbaeb9328142507271a85235e93b9459c98e3
99-zziy <wldud8229@gmail.com>
2022-06-24 12:13:28 +0900

    chapter7 - ex2 문제풀이

---
+++ inflearn/chapter7/ex2.js
@@ -0,0 +1,28 @@
+/*
+- 선택 정렬
+
+N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요. 
+정렬하는 방법은 선택정렬입니다.
+
+▣ 입력설명
+첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에 있습니다.
+
+▣ 출력설명
+오름차순으로 정렬된 수열을 출력합니다.
+
+*/
+
+const solution = (array) => {
+  let answer = array;
+  for (let i = 0; i < answer.length; i++) {
+    for (let j = 0; j < answer.length - i - 1; j++) {
+      if (answer[j] > answer[j + 1])
+        [answer[j], answer[j + 1]] = [answer[j + 1], answer[j]];
+    }
+  }
+
+  return answer;
+};
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
