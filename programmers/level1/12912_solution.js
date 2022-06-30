d3f30617aeed57b74ae37b9efc23715b2152fead
99-zziy <wldud8229@gmail.com>
2022-06-30 13:11:59 +0900

    문제 수정

--- programmers/level1/12912_solution.js
+++ programmers/level1/12912_solution.js
@@ -1,20 +1,19 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12912
 function solution(a, b) {
-    let bigNum = a ;
-    let smallNum = b;
-    let answer = 0;
+  let bigNum = a;
+  let smallNum = b;
+  let answer = 0;
 
-    if (a < b) {
-        bigNum = b;
-        smallNum = a;   
+  if (a < b) {
+    bigNum = b;
+    smallNum = a;
+  }
+  if (a === b) answer = a;
+  else {
+    for (let i = smallNum; i <= bigNum; i++) {
+      answer = answer + i;
     }
-    if (a === b)
-        answer = a;
-    else {
-        for (i = smallNum; i<= bigNum; i++){
-            answer = answer + i
-        }   
-    }
+  }
 
-    return answer;
+  return answer;
 }
