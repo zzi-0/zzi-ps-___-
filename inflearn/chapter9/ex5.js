c0c594872db654447bcae003841f170287b5be4d
99-zziy <wldud8229@gmail.com>
2022-06-30 15:33:32 +0900

    chapter9 - ex5 문제풀이

---
+++ inflearn/chapter9/ex5.js
@@ -0,0 +1,22 @@
+/*
+- 이진트리 넓이우선탐색(BFS)
+
+*/
+
+function solution() {
+  let answer = [];
+  let queue = [];
+  queue.push(1);
+
+  while (queue.length) {
+    let v = queue.shift();
+    answer += v + " ";
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
+    }
+  }
+  return answer;
+}
+
+console.log(solution());
