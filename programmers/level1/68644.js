60336dda4469cb0a2f162294ea74e5c56671c5cc
99-zziy <wldud8229@gmail.com>
2022-07-04 12:58:25 +0900

    프로그래머스 68644 문제풀이

---
+++ programmers/level1/68644.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = [];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            const sum = numbers[i] + numbers[j];
+            if (!answer.includes(sum)) answer.push(sum);
+        }
+    }
+    return answer.sort((a, b) => a - b);
+}
+console.log(solution([2, 1, 3, 4, 1]));
