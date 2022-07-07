1370bfb62484bcd3a56c45cdaf6cdb8c401111bb
99-zziy <wldud8229@gmail.com>
2022-07-07 15:26:42 +0900

    프로그래머스 12943 풀이 수정

--- programmers/level1/12943.js
+++ programmers/level1/12943.js
@@ -1,11 +1,10 @@
 function solution(num) {
     var answer = 0;
     while (num !== 1) {
-        console.log(num % 2);
         if (num % 2 === 0) num = num / 2;
         else if (num % 2 === 1) num = 3 * num + 1;
         answer++;
-        if (answer === 5000) {
+        if (answer === 500) {
             num = 1;
             answer = -1;
         }
