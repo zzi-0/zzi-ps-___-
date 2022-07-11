2bf9a20beade105ad2d7218319c98299c3604d61
99-zziy <wldud8229@gmail.com>
2022-07-11 23:35:41 +0900

    릿코드 two-sum 문제풀이

---
+++ leetcode/two-sum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+    let answer = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) return [i, j];
+        }
+    }
+    return answer;
+};
+
+console.log(twoSum([3, 2, 4], 6));
