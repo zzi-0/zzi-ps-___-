9871d6ca0754702d4c99ebaf71306711cfc4884a
99-zziy <wldud8229@gmail.com>
2022-08-19 23:47:13 +0900

    소수 검사 코드 최적화

--- utils/isPrime.js
+++ utils/isPrime.js
@@ -1,7 +1,7 @@
 // 소수인지 검사하는 코드
 const isPrime = (num) => {
-    if (num === 1 || num === 0) return false;
-    for (let i = 2; i < num; i++) {
+    if (num === 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
     return true;
