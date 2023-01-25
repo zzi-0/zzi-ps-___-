513c06c67bb0caafab9762afabf2aed5ed390973
강지영 <kangjiyoung@gangjiyeong-ui-MacBookPro.local>
2023-01-25 21:15:06 +0900

    feat: 1956 문제풀이

---
+++ baekjoon/shortest-path/1956.py
@@ -0,0 +1,31 @@
+
+V, E = 3, 4
+roads = [[1, 2, 1],
+[3, 2, 1],
+[1, 3, 5],
+[2, 3, 2]]
+INF = int(1e9)
+ans = INF
+
+graph = [[INF for _ in range(V+1)] for _ in range(V+1)]
+
+for i in range(V):
+    graph[i][i] = 0
+
+for [s,e,c] in roads:
+    graph[s][e] = c
+
+for k in range(V+1):  
+    for i in range(V+1):  
+        for j in range(V+1): 
+            if graph[i][k] + graph[k][j] < graph[i][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+for i in range(1,V+1):
+    for j in range(i+1,V+1):
+        ans = min(ans,graph[i][j] + graph[j][i])
+
+if ans == INF:
+    print(-1)
+else:
+    print(ans)
