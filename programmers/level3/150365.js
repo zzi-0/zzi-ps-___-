4c339db43d17ff2eaa9589e9effafb655de5148b
99-zziy <wldud8229@gmail.com>
2023-01-27 21:31:56 +0900

    feat: 150365 dfs로 문제풀이

--- programmers/level3/150365.js
+++ programmers/level3/150365.js
@@ -1,35 +1,34 @@
 function solution(n, m, x, y, r, c, k) {
-    let answer = '';
+    let answer = 'impossible';
     const dx = [1, 0, 0, -1];
     const dy = [0, -1, 1, 0];
+    let isDone = false
 
-    const queue = [];
-    queue.push([x-1,y-1,''])
-
-    function bfs() {
-        while(queue.length) {
-            const [currentX,currentY,path] = queue.pop();
-            if (path.length === k && currentX === r-1 && currentY === c-1) {
-                answer = path;
-                break;
-            }
-            if (path.length < k) {
-                for (let i = 0; i< 4; i++){
-                    const nx = currentX + dx[i]
-                    const ny = currentY + dy[i]
-                    if (0 <= nx && nx < n && 0 <= ny && ny < m){
-                        if (i === 0) queue.unshift([nx,ny,path+'d'])
-                        if (i === 1) queue.unshift([nx,ny,path+'l'])
-                        if (i === 2) queue.unshift([nx,ny,path+'r'])
-                        if (i === 3) queue.unshift([nx,ny,path+'u'])
-                    }
+    function dfs(currentX,currentY,path) {
+        if (k < path.length + Math.abs(currentX - r) + Math.abs(currentY - c)) return
+        if (isDone) return
+        if (path.length === k && currentX === r && currentY === c) {
+            answer = path
+            isDone = true
+        } else if (path.length < k) {
+            for (let i = 0; i < 4; i++){
+                const nx = currentX + dx[i]
+                const ny = currentY + dy[i]
+                if (1 <= nx && nx <= n && 1 <= ny && ny <= m) {
+                    if (i === 0) dfs(nx,ny,path+'d')
+                    if (i === 1) dfs(nx,ny,path+'l')
+                    if (i === 2) dfs(nx,ny,path+'r')
+                    if (i === 3) dfs(nx,ny,path+'u')
                 }
             }
         }
     }
 
-    bfs()
-    return answer || 'impossible';
+    if ((k - (Math.abs(x - r) + Math.abs(y - c))) % 2 === 1) return "impossible"
+    dfs(x,y,'')
+    return answer;
 }
 
+console.log(solution(3,4,2,3,3,1,5))
 console.log(solution(2,2,1,1,2,2,2))
+// console.log(solution(3,3,1,2,3,3,4))
