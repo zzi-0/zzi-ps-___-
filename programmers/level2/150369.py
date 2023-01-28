bade2902d559e07e78aba12f3ffeb2949447be02
99-zziy <wldud8229@gmail.com>
2023-01-28 22:04:43 +0900

    feat: 2번째 시도 시간초과

--- programmers/level2/150369.py
+++ programmers/level2/150369.py
@@ -1,6 +1,5 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    current_cap = cap
     
     remain_deliveries = 0
     for i in deliveries:
@@ -9,53 +8,43 @@ def solution(cap, n, deliveries, pickups):
     remain_pickups = 0
     for i in pickups:
         remain_pickups += i
-
+        
     while 1:
-        n = len(deliveries)
-        temp_current_cap = min(cap,remain_deliveries)
-        temp_deliveries = [0 for _ in range(n)]
-        for i in range(n-1,-1,-1):
-            if temp_current_cap >= deliveries[i]:
-                temp_deliveries[i] = deliveries[i]
-                temp_current_cap -= deliveries[i]
+        current_cap = cap
+        delivery_max_index = 0
+
+        for i in range(n):
+            delivery_max_index = i
+            if current_cap >= deliveries[i]:
+                current_cap -= deliveries[i] 
+                remain_deliveries -= deliveries[i] 
+                deliveries[i] = 0
             else:
-                temp_deliveries[i] = temp_current_cap
-                temp_current_cap = 0
+                remain_deliveries -= current_cap
+                deliveries[i] -= current_cap
+                current_cap = 0
+            if current_cap == 0:
                 break
         
-        max_index = 0
-        for i in range(n):
-            if temp_deliveries[i] != 0:
-                max_index = i
-            deliveries[i] -= temp_deliveries[i]
-            remain_deliveries -= temp_deliveries[i]
-            current_cap -= temp_deliveries[i]
-        
-        for i in range(n-1,-1,-1):
+        for i in range(delivery_max_index-1,-1,-1):
             if current_cap + pickups[i] <= n:
-                current_cap += pickups[i]
-                remain_pickups -= pickups[i]
+                current_cap += pickups[i] 
+                remain_pickups -= pickups[i] 
                 pickups[i] = 0
             else:
-                current_cap += n - current_cap
-                pickups[i] -= n - current_cap
-                remain_pickups -= n - current_cap
+                current_cap += n - pickups[i]
+                remain_pickups -= n - pickups[i]
+                pickups[i] -= n - pickups[i]
             if current_cap == n:
                 break
 
-        answer += 2*(max_index+1)
-
-        if remain_deliveries == 0 and remain_pickups == 0:
+        answer += 2 * (delivery_max_index + 1)
+        if remain_pickups == 0 and remain_deliveries == 0:
             break
-
-        for i in range(n-1,-1,-1):
-            if deliveries[i] == 0 and pickups[i] == 0:
-                deliveries.pop()
-                pickups.pop()
-            else:
-                break
     
+
     return answer
 
 
-print(solution(2,7,[1, 0, 2, 0, 1, 0, 2],[0, 2, 0, 1, 0, 2, 0]))
+print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))
+print(solution(2, 7, [1, 0, 2, 0, 1, 0, 2],[0, 2, 0, 1, 0, 2, 0]))
