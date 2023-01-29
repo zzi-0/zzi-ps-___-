da3bd2127786cd3d81ce367bd99c38ffcc56dda8
99-zziy <wldud8229@gmail.com>
2023-01-29 19:13:41 +0900

    feat: 150369 문제풀이

--- programmers/level2/150369.py
+++ programmers/level2/150369.py
@@ -1,17 +1,20 @@
 def solution(cap, n, deliveries, pickups):
-
     deliveries = deliveries[::-1]
+    pickups = pickups[::-1]
     answer = 0
-    d = 0
-    
-    for i in range(n):
-        print(d,answer)
-        d += deliveries[i]
-        
-        while d > 0:
-            d -= cap
-            answer += (n - i) * 2
-    return answer
 
+    have_to_deli = 0
+    have_to_pick = 0
+
+    for i in range(n):
+        have_to_deli += deliveries[i]
+        have_to_pick += pickups[i]
+
+        while have_to_deli > 0 or have_to_pick > 0:
+            have_to_deli -= cap
+            have_to_pick -= cap
+            answer += (n - i) * 2
+
+    return answer
 print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))
 print(solution(2, 7, [1, 0, 2, 0, 1, 0, 2],[0, 2, 0, 1, 0, 2, 0]))
