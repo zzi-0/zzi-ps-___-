850430b0ceb0ef966a58765a0993a72c441c2cb8
99-zziy <wldud8229@gmail.com>
2023-02-05 21:10:11 +0900

    feat: 1062 문제풀이

--- baekjoon/brute-force/1062.py
+++ baekjoon/brute-force/1062.py
@@ -1,42 +1,36 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-sys.setrecursionlimit(10**8)
-
-n, k = map(int, input().split())
-words = [input()[4:-4] for _ in range(n)]
+n, k = 3, 6
+words = ['antarctica',
+'antahellotica',
+'antacartica']
 learn = [0] * 26
 max = 0
 
 for c in ('a', 'c', 'i', 'n', 't'):
     learn[ord(c) - ord('a')] = 1
 
-def get_readable_cnt():
-    cnt = 0
-    global max
-    for word in words:
-        readable = True
-        for c in word:
-            if not learn[ord(c) - ord('a')]:
-                readable = False
-                break
-        if readable:
-            cnt += 1
-    return cnt
-    
-def dfs(level):
+def dfs(idx,level):
     global max
     if level == k-5:
-        cnt = get_readable_cnt()
+        cnt = 0
+        for word in words:
+            readable = True
+            for c in word:
+                if not learn[ord(c) - ord('a')]:
+                    readable = False
+                    break
+            if readable:
+                cnt += 1
         if max < cnt:
             max = cnt
         return
     else:
-        for i in range(26):
+        for i in range(idx,26):
             if not learn[i]:
                 learn[i] = 1
-                dfs(level+1)
+                dfs(i,level+1)
                 learn[i] = 0
 
 if k < 5:
@@ -44,5 +38,6 @@ if k < 5:
 elif k == 26:
     print(n)
 else:
-    dfs(0)
+    dfs(0,0)
     print(max)
+
