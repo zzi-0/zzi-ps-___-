06b2cae977abd826df1c39ed0fc7ff9d91760707
99-zziy <wldud8229@gmail.com>
2023-02-15 00:03:56 +0900

    feat: 72414 문제풀이

--- programmers/level3/72414.py
+++ programmers/level3/72414.py
@@ -1,59 +1,50 @@
 def solution(play_time, adv_time, logs):
-    answer = ''
-    answer_int = 0
-    max = 0
-    n = len(logs)
-    [play_hour,play_min,play_sec] = play_time.split(':')
-    [adv_hour,adv_min,adv_sec] = adv_time.split(':')
-    play_time = int(play_hour) * 60 * 60 + int(play_min) * 60 + int(play_sec)
-    adv_time = int(adv_hour) * 60 * 60 + int(adv_min) * 60 + int(adv_sec)
-    original = []
-    play_list = []
-    
-    original.append('00:00:00')
-    play_list.append((0,adv_time))
-    for i in range(n):
-        [i_start_time,i_end_time] = logs[i].split("-")
-        [i_start_hour,i_start_min,i_start_sec] = i_start_time.split(':')
-        [i_end_hour,i_end_min,i_end_sec] = i_end_time.split(':')
-        adv_start = int(i_start_hour) * 60 * 60 + int(i_start_min) * 60 + int(i_start_sec)
-        adv_end = int(i_end_hour) * 60 * 60 + int(i_end_min) * 60 + int(i_end_sec)
-        play_list.append((adv_start,adv_end))
-        original.append(i_start_time)
-    
-    for j in range(len(play_list)):
-        play_start,play_end = play_list[j]
-        adv_end = play_start + adv_time
-        sum = 0
-        
-        if adv_end > play_time:
-            adv_end = play_time
-        for another_play_start,another_play_end in play_list:
+    play_time = str_to_int(play_time)
+    adv_time = str_to_int(adv_time)               
+    all_time = [0 for i in range(play_time + 1)]
 
-            if play_start > another_play_end:
-                continue
-            
-            if adv_start >= another_play_start:
-                if another_play_end >= adv_end:
-                    sum += (adv_end - play_start)
-                else:
-                    sum += (another_play_end - play_start)
-            else:
-                if another_play_end >= adv_end:
-                    sum += (adv_end - another_play_start)
-                else:
-                    sum += (another_play_end - another_play_start)
-
-        if max < sum:
-            max = sum
-            answer_int = play_start
-        elif max == sum:
-            if answer_int > play_start:
-                answer_int = play_start
-                answer = original[j]
+    for l in logs:                          
+        start, end = l.split('-')
+        start = str_to_int(start)
+        end = str_to_int(end)
+        all_time[start] += 1
+        all_time[end] -= 1
     
-    
-    return answer
+    for i in range(1, len(all_time)):
+        all_time[i] += all_time[i - 1]
 
+    for i in range(1, len(all_time)):
+        all_time[i] += all_time[i - 1]
+    
+    max_time = 0
+    most_view = 0
+
+    for i in range(adv_time-1,play_time):
+        if i <= adv_time:
+            if most_view < all_time[i]:
+                most_view = all_time[i]
+                max_time = i - adv_time + 1
+        else:
+            if most_view < all_time[i] - all_time[i-adv_time]:
+                most_view =  all_time[i] - all_time[i-adv_time]
+                max_time = i - adv_time + 1
+    
+    return int_to_str(max_time)
+
+def str_to_int(time):
+    h, m, s = time.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+
+def int_to_str(time):
+    h = time // 3600
+    h = '0' + str(h) if h < 10 else str(h)
+    time = time % 3600
+    m = time // 60
+    m = '0' + str(m) if m < 10 else str(m)
+    time = time % 60
+    s = '0' + str(time) if time < 10 else str(time)
+    return h + ':' + m + ':' + s
 
 print(solution("02:03:55", "00:14:15", ["01:20:15-01:45:14", "00:25:50-00:48:29", "00:40:31-01:00:00", "01:37:44-02:02:30", "01:30:59-01:53:29"]))
+print(solution("99:59:59", "25:00:00", ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]))
