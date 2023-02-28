e4f46e77b577fb73fbf7add4c02511a14e756b99
99-zziy <wldud8229@gmail.com>
2023-02-28 12:29:43 +0900

    feat: 2661 문제풀이

--- baekjoon/back-tracking/2661.py
+++ baekjoon/back-tracking/2661.py
@@ -1,24 +1,27 @@
 import sys
 from itertools import product
 
-n = int(sys.stdin.readline())
+""" n = int(sys.stdin.readline()) """
+n = 10
+arr = []
 
-check = []
-for i in range(1,4):
-    for comb in product([1,2,3], repeat=i):  
-        num = "".join(map(str, comb))
-        num += num
-        check.append(num)
+def check(arr,idx):
+    for i in range(1,(idx//2) +1):
+            if arr[-i:] == arr[-2 * i:-i]:
+                return False
+    else:
+        return True
 
+def dfs(idx):
+    if not check(arr,idx):
+        return
+    if idx == n+1:
+        print(int("".join(map(str,arr))))
+        exit()
+    
+    for i in range(1,4):
+        arr.append(i)
+        dfs(idx+1)
+        arr.pop()
 
-for comb in product([1,2,3], repeat=n):
-    comb = list(comb)
-    num = "".join(map(str, comb))
-    is_good_num = True  
-    for c in check:
-        if c in num:
-            is_good_num = False
-            break
-    if is_good_num:
-        print(num)
-        break
+dfs(1)
