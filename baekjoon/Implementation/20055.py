63d8de1c2334924f33078d89cef5fef35fe86017
99-zziy <wldud8229@gmail.com>
2023-03-29 12:10:59 +0900

    feat: 20055 deque를 활용하여 시간초과 해결

--- baekjoon/Implementation/20055.py
+++ baekjoon/Implementation/20055.py
@@ -1,48 +1,32 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-n, k = map(int,input().split())
-belt = list(map(int, input().split()))
-robot = [0] * (2*n)
+n, k = 5, 8
+belt = deque([100, 99, 60, 80, 30, 20, 10, 89, 99, 100])
+robot = deque([0] * (2*n))
 ans = 0
-
-def is_stop(belt,k):
-    count = 0
-    for i in range(len(belt)):
-        if belt[i] == 0:
-            count += 1
-        if count >= k:
-            return True
-    return False
 
 while True:
     ans+=1
-    rotated_robot = [0]*(2*n)
-    rotated_belt = [0]*(2*n)
-    for i in range(2*n):
-        if i == 0:
-            rotated_robot[i] = 0
-            rotated_belt[i] = belt[2*n-1]
-        else:
-            rotated_robot[i] = robot[i-1]
-            rotated_belt[i] = belt[i-1]
+    robot.rotate(1)
+    belt.rotate(1)
+    robot[2*n-1] = 0
             
     for i in range(n-1,-1,-1):
-        if i == n-1 and rotated_robot[i]:
-            rotated_robot[i] = 0
+        if i == n-1 and robot[i]:
+            robot[i] = 0
         else:
-            if rotated_robot[i+1] == 0 and rotated_belt[i+1] >= 1 and rotated_robot[i]:
-                rotated_robot[i+1] = 1
-                rotated_robot[i] = 0
-                rotated_belt[i+1] -= 1
+            if robot[i+1] == 0 and belt[i+1] >= 1 and robot[i]:
+                robot[i+1] = 1
+                robot[i] = 0
+                belt[i+1] -= 1
 
-    if rotated_belt[0] and rotated_robot[0] == 0:
-        rotated_robot[0] = 1
-        rotated_belt[0] -= 1
+    if belt[0] and robot[0] == 0:
+        robot[0] = 1
+        belt[0] -= 1
 
-    if is_stop(rotated_belt,k):
+    if belt.count(0) >= k:
         print(ans)
         break
         
-    robot = rotated_robot[:]
-    belt = rotated_belt[:]
