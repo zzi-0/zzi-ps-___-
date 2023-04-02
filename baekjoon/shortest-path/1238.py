2da1dac3b0d6aa99837c5979533aa0c22f65b2b0
99-zziy <wldud8229@gmail.com>
2023-04-02 11:27:35 +0900

    feat: 다익스트라로 시간초과 해결

--- baekjoon/shortest-path/1238.py
+++ baekjoon/shortest-path/1238.py
@@ -1,4 +1,6 @@
-n, m, x = 4, 8, 2
+import heapq
+
+n, m, end = 4, 8, 4
 roads = [[1, 2, 4],
 [1, 3, 2],
 [1, 4, 7],
@@ -8,26 +10,36 @@ roads = [[1, 2, 4],
 [3, 4, 4],
 [4, 2, 3]]
 
-INF = int(1e9)
 ans = 0
+INF = int(1e9)
+coming_distance = [INF]*(n+1)
+graph = [[] for _ in range(n+1)]
 
-graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+for i in range(m):
+    s,e,c = roads[i]
+    graph[s].append((e,c))
 
+def dijkstra(start,distance):
+    q = []
+    heapq.heappush(q,(0,start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for s,c in graph[now]:
+            cost = dist + c
+            if cost < distance[s]:
+                distance[s] = cost
+                heapq.heappush(q,(cost,s))
+
+# 2(파티) -> 집 : 집 가는 길
+dijkstra(end,coming_distance)
+
+# 집 -> 2(파티) : 파티 가는 길
 for i in range(1,n+1):
-    graph[i][i] = 0
-
-for [s,e,c] in roads:
-    graph[s][e] = c
-
-for k in range(n+1):  
-    for i in range(n+1):  
-        for j in range(n+1): 
-            if graph[i][k] + graph[k][j] < graph[i][j]:
-                graph[i][j] = graph[i][k] + graph[k][j]
-
-for i in range(1,n+1):
-    if ans < graph[i][x] + graph[x][i]:
-        ans = graph[i][x] + graph[x][i]
-
+    going_distance = [INF]*(n+1)
+    dijkstra(i,going_distance)
+    if going_distance[end] + coming_distance[i] > ans:
+        ans = going_distance[end] + coming_distance[i]
 print(ans)
-
