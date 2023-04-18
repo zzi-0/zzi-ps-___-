bc72692394f6a962f30ff806ea762c88b6a0d9ab
99-zziy <wldud8229@gmail.com>
2023-04-18 11:55:25 +0900

    feat: 22860 문제풀이

--- baekjoon/Implementation/22860.py
+++ baekjoon/Implementation/22860.py
@@ -1,69 +1,30 @@
 import sys
 sys.setrecursionlimit(10**8)
-
 input = sys.stdin.readline
 
-n, m = 3, 4
-infos = [['main', 'FolderA', 1],
-['main', 'FolderB', 1],
-['FolderA', 'File1', 0],
-['FolderA', 'File2', 0],
-['FolderB', 'FolderC', 1],
-['FolderB', 'File1', 0],
-['FolderB', 'File3', 0]]
+dict = {}
 
-q = 3
-folders = ['main','main/FolderA','main/FolderB','main/FolderB/FolderC']
-
-files = {}
-parent_folder = {}
-folder_in_files = [[] for _ in range(q)]
-ans = [[0] * 2 for _ in range(q)]
-
-# parent_folder에 각각 상위 폴더 적어두고,
-# infos 돌리면서 상위 폴더의 상위 폴더가 있는지 재귀적으로 확인 총 결과 -> '/'로 붙이기 그래서 folders에 index 찾아서 파일 넣어두기,
-
+def check(target, files):
+    if target not in dict:
+        return
+    for name, type in dict[target]:
+        if type == 0:
+            files.append(name)
+        else:
+            check(name, files)
+            
+n, m = map(int, input().split())
 for i in range(n+m):
-    [p,f,c]= infos[i]
-    if c == 1:
-        parent_folder[f] = p
+    p,f,c = input().rstrip().split()
+    if p not in dict:
+        dict[p] = []
+        dict[p].append([f, int(c)])
+    else:
+        dict[p].append([f, int(c)])
 
-print(parent_folder)
-
-def find_parent_folder(p,ans):
-    parent_parent_folder = parent_folder.get(p)
-    # print('ans',ans)
-    if parent_parent_folder:
-        return find_parent_folder(parent_parent_folder,parent_parent_folder+'/'+ans)
-    return ans
-
-for i in range(n+m):
-    [p,f,c]= infos[i]
-    folder = find_parent_folder(p,p)
-    
-    # print('1 full folder',folder)
-    # print("c is ",c)
-    if c == 1:
-        folder += '/'+f
-    print('folder',folder)
-    # print('')
-    if folder in folders:
-        folder_index = folders.index(folder)
-        if c == 0:
-            folder_in_files[folder_index].append(f)
-
-# print(folder_in_files)
-
-for i in range(q):
-    for j in range(i+1,q):
-        if folders[i]+'/' in folders[j]:
-            for f in folder_in_files[j]:
-                folder_in_files[i].append(f)
-
-for i in range(q):
-    f = folder_in_files[i]
-    ans[i] = [len(set(f)),len(f)]
-# print(folder_in_files,ans)
-
-for a in ans:
-    print(a[0], a[1])
+q = int(input())
+for _ in range(q):
+    folder = input().rstrip().split('/')
+    files = []
+    check(folder[-1], files)
+    print(len(set(files)),len(files))
