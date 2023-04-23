f94e031a8be31b9a97c996461e6f3fb65bd86c09
99-zziy <wldud8229@gmail.com>
2023-04-23 11:37:12 +0900

    feat: 17070 문제풀이

--- baekjoon/graph/17070.py
+++ baekjoon/graph/17070.py
@@ -1,56 +1,29 @@
-import sys
-
-input = sys.stdin.readline
-
-n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
+n = 6
+board = [[0, 0, 0,0,0,0],[0, 1, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0]]
 visited = [[0] * n for _ in range(n)]
 ans = 0
+dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(3)]
 
-def right(visited,x,y,d):
-    if 0 <= x < n and 0 <= y+1 < n and visited[x][y+1] == 0 and board[x][y+1] == 0:
-        visited[x][y+1] = 1
-        dfs(x,y+1,d)
-        visited[x][y+1] = 0
+dp[0][0][1] = 1
+for i in range(2, n):
+    if board[0][i] == 0:
+        dp[0][0][i] = dp[0][0][i - 1]
 
-def down(visited,x,y,d):
-    if 0 <= x+1 < n and 0 <= y < n and visited[x+1][y] == 0 and board[x+1][y] == 0:
-        visited[x+1][y] = 1
-        dfs(x+1,y,d)
-        visited[x+1][y] = 0
-
-def right_down(visited,x,y,d):
-    if 0 <= x+1 < n and 0 <= y+1 < n and visited[x+1][y] == 0 and visited[x+1][y+1] == 0 and visited[x][y+1] == 0 and board[x][y+1] == 0 and board[x+1][y] == 0 and board[x+1][y+1] == 0:
-        visited[x][y+1] = 1
-        visited[x+1][y+1] = 1
-        visited[x+1][y] = 1
-        dfs(x+1,y+1,d)
-        visited[x][y+1] = 0
-        visited[x+1][y+1] = 0
-        visited[x+1][y] = 0
+for r in range(1, n):
+    for c in range(1, n):
+        # 대각선 파이프를 추가하는 과정
+        if board[r][c] == 0 and board[r][c - 1] == 0 and board[r - 1][c] == 0:
+            dp[1][r][c] = dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1]
+            
+        # 가로, 세로 파이프를 추가하는 과정
+        if board[r][c] == 0:
+            dp[0][r][c] = dp[0][r][c - 1] + dp[1][r][c - 1]
+            dp[2][r][c] = dp[2][r - 1][c] + dp[1][r - 1][c]
 
 
-
-def dfs(x,y,d):
-    global ans
-    if x == n-1 and y == n-1:
-        ans += 1
-    if d == 0:
-        right(visited,x,y,0)
-        right_down(visited,x,y,2)
-    if d == 1:
-        down(visited,x,y,1)
-        right_down(visited,x,y,2)
-    if d == 2:
-        right(visited,x,y,0)
-        down(visited,x,y,1)
-        right_down(visited,x,y,2)
-
-
-
-
-
-visited[0][0] = 1
-visited[0][1] = 1
-dfs(0,1,0)
-print(ans)
+# 최종 결과 출력
+print(sum(dp[i][n - 1][n - 1] for i in range(3)))
