ef0f0cebeb50911189c6ca56d75f6a2a70b763f2
99-zziy <wldud8229@gmail.com>
2023-05-10 11:05:24 +0900

    feat: 2632 문제풀이

--- baekjoon/dp/2632.py
+++ baekjoon/dp/2632.py
@@ -1,67 +1,29 @@
 import sys
-sys.setrecursionlimit(10**8)
+from collections import defaultdict
+def input(): return sys.stdin.readline().rstrip()
 
-pizza = int(sys.stdin.readline().rstrip())
-m, n = map(int, sys.stdin.readline().split())
-a = [int(sys.stdin.readline().rstrip()) for _ in range(m)]
-b = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
+def find_case(pizza, length):
+    case = defaultdict(int)
+    for p1 in range(length):
+        stack = []
+        for p2 in range(length):
+            p = (p1 + p2) % length
+            stack.append(pizza[p])
+            case[sum(stack)] += 1
+    case[sum(pizza)] = 1 
+    return case
 
+k = int(input())
+n, m = map(int,input().split())
+pizza_a = [int(input()) for _ in range(n)]
+pizza_b = [int(input()) for _ in range(m)]
 
-a_visited = [0] * (m)
-b_visited = [0] * (n)
-ans = 0
+case1 = find_case(pizza_a, n)
+case2 = find_case(pizza_b, m)
 
+result = case1.get(k, 0) + case2.get(k, 0)
+for num in case1:
+    if k-num in case2:
+        result += case1[num] * case2[k-num]
 
-def dfs(type,c,sum):
-    global ans
-    if sum == pizza:
-        ans += 1
-        return
-    
-    if type == 'a':
-        nc = (c + 1) % m
-        if a_visited[nc] == 0 and sum + a[nc] <= pizza:
-            a_visited[nc] = 1
-            dfs('a',nc,sum+a[nc])
-            a_visited[nc] = 0
-        if 1 not in b_visited:
-            for i in range(n):
-                nc = (c + i) % n
-                if b_visited[nc] == 0 and sum + b[nc] <= pizza:
-                    b_visited[nc] = 1
-                    dfs('b',nc,sum+b[nc])
-                    b_visited[nc] = 0
-
-    
-    if type == 'b':
-        nc = (c + 1) % n
-        if b_visited[nc] == 0 and sum + b[nc] <= pizza:
-            b_visited[nc] = 1
-            dfs('b',nc,sum+b[nc])
-            b_visited[nc] = 0
-        if 1 not in a_visited:
-            for i in range(m):
-                nc = (c + i) % m
-                if a_visited[nc] == 0 and sum + a[nc] <= pizza:
-                    a_visited[nc] = 1
-                    dfs('a',nc,sum+a[nc])
-                    a_visited[nc] = 0
-
-
-for i in range(m):
-    a_visited[i] = 1
-    dfs('a',i,a[i])
-    a_visited[i] = 0
-
-for i in range(n):
-    b_visited[i] = 1
-    dfs('a',i,b[i])
-    b_visited[i] = 0
-
-print(ans)
-
-""" 0 4 7
-0 1 7
-2 5
-3
-0 1 2 4 """
+print(result)
