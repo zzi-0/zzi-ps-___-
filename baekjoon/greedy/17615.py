af473f640e545b9e452b5bce0a27ea5a70cec9ee
99-zziy <wldud8229@gmail.com>
2023-06-21 12:23:29 +0900

    feat: 17615 문제풀이

---
+++ baekjoon/greedy/17615.py
@@ -0,0 +1,24 @@
+# 11:35
+n = 8
+balls = 'RRRRRBBR'
+cnt = []
+
+# 우측으로 레드 모으기
+rexplore = balls.rstrip('R')
+cnt.append(rexplore.count('R'))
+
+# 우측으로 블루 모으기
+rexplore = balls.rstrip('B')
+cnt.append(rexplore.count('B'))
+
+# 좌측으로 레드 모으기
+lexplore = balls.lstrip('R')
+cnt.append(lexplore.count('R'))
+
+# 좌측으로 블루 모으기
+lexplore = balls.lstrip('B')
+cnt.append(lexplore.count('B'))
+
+print(min(cnt))
+
+
