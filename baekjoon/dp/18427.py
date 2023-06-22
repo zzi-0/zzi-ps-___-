44ec8a8e8309aed7e279b27f144e00968bb6c28c
99-zziy <wldud8229@gmail.com>
2023-06-22 20:31:50 +0900

    feat: 18427 문제풀이

---
+++ baekjoon/dp/18427.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n, m, h = map(int, input().split())
+dp = [[0 for _ in range(h+1)] for _ in range(n+1)]
+
+dp[0][0] = 1
+for i in range(1,n+1):
+    dp[i] = dp[i-1].copy()
+    blocks = list(map(int, input().split()))
+    for b in blocks:
+        for j in range(b, h+1):
+            dp[i][j]+=dp[i-1][j-b]
+
+print(dp[n][h]%10007)
