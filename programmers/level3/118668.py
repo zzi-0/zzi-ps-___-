357438315334cf98aada18e46846fb8cc12eb33d
99-zziy <wldud8229@gmail.com>
2023-08-19 16:16:13 +0900

    feat: 118668 다시 풀어보기

--- programmers/level3/118668.py
+++ programmers/level3/118668.py
@@ -1,38 +1,40 @@
 def solution(alp, cop, problems):
-  alp_tgt = 0
-  cop_tgt = 0
+  max_alp = alp
+  max_cop = cop
   
-  for problem in problems:
-    a,c,_,_,_ = problem
-    if alp_tgt < a:
-      alp_tgt = a
-    if cop_tgt < c:
-      cop_tgt = c
+  for [alp_req, cop_req, alp_rwd, cop_rwd, cost] in problems:
+    if max_alp < alp_req:
+      max_alp = alp_req
+    if max_cop < cop_req:
+      max_cop = cop_req
   
-  if alp >= alp_tgt and cop >= cop_tgt:
+  if alp >= max_alp and cop >= max_cop:
     return 0
-    
-  alp_tgt = max(alp_tgt,alp)
-  cop_tgt = max(cop_tgt,cop)
   
   problems.append([0,0,1,0,1])
   problems.append([0,0,0,1,1])
-  dp = [[100000 for _ in range(cop_tgt+1)] for _ in range(alp_tgt+1)]
-  dp[alp][cop] = 0
 
-  for r in range(alp,alp_tgt+1):
-    for c in range(cop,cop_tgt+1):
-      for alp_req,cop_req,alp_rwd,cop_rwd,cost in problems:
-        if alp_req <= r and cop_req <= c:
-          nr,nc = r+alp_rwd, c+cop_rwd
-          if nr > alp_tgt:
-            nr = alp_tgt
-          if nc > cop_tgt:
-            nc = cop_tgt
-          dp[nr][nc] = min(dp[nr][nc],dp[r][c] + cost)
+  board = [[10000 for _ in range(max_cop+1)] for _ in range(max_alp+1)]
+  board[alp][cop] = 0
+
+  for x in range(alp, max_alp+1):
+    for y in range(cop, max_cop+1):
+      for [alp_req, cop_req, alp_rwd, cop_rwd, cost] in problems:
+        if alp_req <= x and cop_req <= y:
+          nx = x + alp_rwd
+          ny = y + cop_rwd
+
+          if max_alp < nx:
+            nx = max_alp
+          if max_cop < ny:
+            ny = max_cop
+          
+          board[nx][ny] = min(board[nx][ny],board[x][y] + cost)
   
-  # print(dp)
-  return dp[alp_tgt][cop_tgt]
+  return board[max_alp][max_cop]
+
+
+  
 
 print(solution(10,10,[[10,15,2,1,2],[20,20,3,3,4]]))
 print(solution(0,0,[[0,0,2,1,2],[4,5,3,1,2],[4,11,4,0,2],[10,4,0,4,2]]))
